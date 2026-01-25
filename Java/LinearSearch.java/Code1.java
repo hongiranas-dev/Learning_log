49cd4582d06a1b1f801079f7546c46dc0ccf18d3
hongirans-dev <hongiranahonnu04@gmail.com>
2026-01-25 16:38:57 +0530

    linear search basic code

---
+++ Java/LinearSearch.java/Code1.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Code1 {
+  public static void main(String[] args) {
+  Scanner sc=new Scanner(System.in);
+    System.out.println("enter the target");
+    int target=sc.nextInt();
+     int  a= Linear( target);
+     System.out.println(a);
+    sc.close();
+  }
+  static int Linear(int target){
+  Scanner sc=new Scanner(System.in);
+    int arr[]={1,2,3,4,5};
+    
+    for(int i=0;i<arr.length;i++){
+    if(target==arr[i]){
+      return i;
+    }
+    
+  } sc.close();
+    return -1;
+    
+ 
+    }
+  }
+  
+
