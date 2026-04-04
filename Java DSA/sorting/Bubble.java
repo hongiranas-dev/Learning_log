188cbcc855c498961f62c08183f9669c246ff74c
hongirans-dev <hongiranahonnu04@gmail.com>
2026-04-04 11:28:09 +0530

    new sorting files

--- Java DSA/sorting/Bubble.java
+++ Java DSA/sorting/Bubble.java
@@ -4,7 +4,7 @@ public class Bubble {
    System.out.println("Bubble Sort");
    int arr[] = {5, 1, 4, 2, 8};
    for(int i=0;i<arr.length;i++){
-    for(int j=1;j<arr.length-i;j++){
+    for(int j=0;j<arr.length-1-i;j++){
       if(arr[j]>arr[j+1]){
         int temp=arr[j+1];
         arr[j+1]=arr[j];
@@ -15,4 +15,3 @@ public class Bubble {
   System.out.println("Sorted Array: " + Arrays.toString(arr));
 }
 }
-  
