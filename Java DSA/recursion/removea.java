7ff6ed469b47a2820d6d3d299205e90fdca42444
Hongirana S <hongiranahonnu04@gmail.com>
2026-06-12 20:15:40 +0530

    strings with recursion

--- Java DSA/recursion/removea.java
+++ Java DSA/recursion/removea.java
@@ -1,5 +1,20 @@
 public class removea {
   public static void main(String[] args) {
+    StringBuilder s=new StringBuilder("abbbbbaacdrtia");
+    removechar(s,0);
+    System.out.println(s);
+  }
+  static void removechar(StringBuilder s,int i ){
+    if(i==s.length()){
+      return;
+    }
+    if(s.charAt(i)=='a'){
+      s.deleteCharAt(i);
+      removechar( s,i);
+    }
+    else{
+      removechar(s, i+1);
+    }
     
   }
 }
